fb68740648b9e4dea5c162add44d96c01615341a
nirilajay <nirilajay2317@gmail.com>
2022-10-09 16:51:44 +0530

    block.test is created and tested

--- block.js
+++ block.js
@@ -1,17 +1,10 @@
 class Block {
-    constructor({ timestamp, lastHash, hash, data }) {
-        this.timestamp = timestamp;
-        this.lastHash = lastHash;
-        this.hash = hash;
-        this.data = data;
-    }
+     constructor({ timestamp, lastHash, hash, data }) {
+         this.timestamp = timestamp;
+         this.lastHash = lastHash;
+         this.hash = hash;
+         this.data = data;
+     }
 }
 
-const Block1 = new Block({
-    timestamp: '01/01/01', 
-    lastHash: 'nrl-lasthash', 
-    hash: 'nrl-hash', 
-    data: 'nrl-data'
-});
-
-console.log('Block1', Block1);
+module.exports = Block;
