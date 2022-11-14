f7ca2ac3b634f3a73e00a93907c691e583616075
nirilajay <nirilajay2317@gmail.com>
2022-11-14 20:12:26 +0530

    difficulty and nonce value code added but test is running infinitely

--- blockchain.js
+++ blockchain.js
@@ -18,34 +18,33 @@ class Blockchain {
   replaceChain(chain) {
     if (chain.length <= this.chain.length) {
       console.error('The incoming chain must be longer');
+
       return;
     }
 
-    if(!Blockchain.isValidChain(chain)) {
+    if (!Blockchain.isValidChain(chain)) {
       console.error('The incoming chain must be valid');
+
       return;
     }
 
-    console.log('replacing chain with', chain);
+    console.log('replacing chain wiih', chain);
     this.chain = chain;
   }
 
   static isValidChain(chain) {
-    if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
-      return false;
-    };
+    if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
 
-    for(let i=1; i<chain.length; i++)  {
-      const { timestamp, lastHash, hash, data } = chain[i];
+    for (let i=1; i<chain.length; i++) {
+      const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
 
       const actualLastHash = chain[i-1].hash;
 
+      if (lastHash !== actualLastHash) return false;
 
-      if(lastHash !== actualLastHash) return false;
+      const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
 
-      const validatedHash = cryptoHash(timestamp, lastHash, data);
-
-      if(hash !== validatedHash) return false;
+      if (hash !== validatedHash) return false;
     }
 
     return true;
