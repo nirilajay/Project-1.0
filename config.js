dfb07127d99c072eaa15f8b4fd0ee58e2759fe94
nirilajay <nirilajay2317@gmail.com>
2022-11-15 15:42:29 +0530

    difficulty and nonce value code added but test is successfull

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-const GENESIS_DATA ={
+const INITIAL_DIFFICULTY = 3;
+
+const GENESIS_DATA = {
     timestamp: 1,
     lastHash: '----',
     hash: 'nrl-one',
+    difficulty: INITIAL_DIFFICULTY,
+    nonce: 0,
     data: []
 };
 
