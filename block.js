7b6ce30335da0d7a5f472898dccd4b4ff64993b4
nirilajay <nirilajay2317@gmail.com>
2023-04-04 23:46:18 +0530

    the proof of work system is implemented and imporved with prevention of difficulty jumps

--- block.js
+++ block.js
@@ -1,3 +1,4 @@
+const hexToBinary = require('hex-to-binary');
 const { GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
@@ -13,25 +14,28 @@ class Block {
 
   static genesis() {
     return new this(GENESIS_DATA);
-  }
+  } 
 
   static mineBlock({ lastBlock, data }) {
-    let hash, timestamp;
     const lastHash = lastBlock.hash;
-    const { difficulty } = lastBlock;
+    let hash, timestamp;
+    let { difficulty } = lastBlock;
     let nonce = 0;
 
     do {
       nonce++;
       timestamp = Date.now();
+      difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp})
       hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
-    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+    } while (hexToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty));
 
     return new this({ timestamp, lastHash, data, difficulty, nonce, hash });
   }
 
   static adjustDifficulty({ originalBlock, timestamp }) {
     const { difficulty } = originalBlock;
+
+    if (difficulty < 1) return 1;
 
 
     if((timestamp - originalBlock.timestamp) > MINE_RATE ) return difficulty - 1;
