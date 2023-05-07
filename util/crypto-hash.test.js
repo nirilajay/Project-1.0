cb295e5934ecacc4edb2957266a131fdc6bfb875
nirilajay <nirilajay2317@gmail.com>
2023-05-08 00:15:31 +0530

    improved the code for cryptohash

--- util/crypto-hash.test.js
+++ util/crypto-hash.test.js
@@ -2,12 +2,20 @@ const cryptoHash = require('./crypto-hash');
 
 describe('cryptoHash', () => {
    it('generates a SHA-256 hashed output', () => {
-        expect(cryptoHash('nrl'))
-        .toEqual('90b62dcc6a7dcbeca44a43ca5b027c4fb480379617b81fd682bcfe938d8c136d');
+        expect(cryptoHash('foo'))
+        .toEqual('b2213295d564916f89a6a42455567c87c3f480fcd7a1c15e220f17d7169a790b');
     });
 
     it('produces the same hash with the same input arguments in any order', () => {
         expect(cryptoHash('one', 'two', 'three'))
         .toEqual(cryptoHash('three', 'one', 'two'));
+    });
+
+    it('produces a unique hash when the properties have changed on an input', () => { 
+        const foo = {};
+        const originalHash = cryptoHash(foo);
+        foo['a'] = 'a';
+
+        expect(cryptoHash(foo)).not.toEqual(originalHash);
     });
 });
