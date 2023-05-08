5ba267392ca1c6f583b15d2a376f65df74e5b03f
nirilajay <nirilajay2317@gmail.com>
2023-05-08 21:02:22 +0530

    mine transactions endpoint is set

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,22 +1,26 @@
+const Transaction = require('../wallet/transaction');
+
 class TransactionMiner {
-    constructor({ blockchain, transactionPool, wallet, pubsub }) {
-      this.blockchain = blockchain;
-      this.transactionPool = transactionPool;
-      this.wallet = wallet;
-      this.pubsub = pubsub;
-    }
-  
-    mineTransactions() {
-      // get the transaction pool's valid transactions
-  
-      // generate the miner's reward
-  
-      // add a block consisting of these transactions to the blockchain
-  
-      // broadcast the updated blockchain
-  
-      // clear the pool
-    }
+  constructor({ blockchain, transactionPool, wallet, pubsub }) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
+    this.pubsub = pubsub;
   }
-  
-  module.exports = TransactionMiner;
+
+  mineTransactions() {
+    const validTransactions = this.transactionPool.validTransactions();
+
+    validTransactions.push(
+      Transaction.rewardTransaction({ minerWallet: this.wallet })
+    );
+
+    this.blockchain.addBlock({ data: validTransactions });
+
+    this.pubsub.broadcastChain();
+
+    this.transactionPool.clear();
+  }
+}
+
+module.exports = TransactionMiner;
