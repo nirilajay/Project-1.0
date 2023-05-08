6c8b674ba9d1f05bf33798bebe8e1f717a77a8d4
nirilajay <nirilajay2317@gmail.com>
2023-05-08 20:35:28 +0530

    reward transaction is set

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,21 +1,29 @@
+const Transaction = require('./transaction');
+
 class TransactionPool {
-    constructor() {
-      this.transactionMap = {};
-    }
-  
-    setTransaction(transaction) {
-      this.transactionMap[transaction.id] = transaction;
-    }
-  
-    setMap(transactionMap) {
-      this.transactionMap = transactionMap;
-    }
-  
-    existingTransaction({ inputAddress }) {
-      const transactions = Object.values(this.transactionMap);
-  
-      return transactions.find(transaction => transaction.input.address === inputAddress);
-    }
+  constructor() {
+    this.transactionMap = {};
   }
-  
-  module.exports = TransactionPool;
+
+  setTransaction(transaction) {
+    this.transactionMap[transaction.id] = transaction;
+  }
+
+  setMap(transactionMap) {
+    this.transactionMap = transactionMap;
+  }
+
+  existingTransaction({ inputAddress }) {
+    const transactions = Object.values(this.transactionMap);
+
+    return transactions.find(transaction => transaction.input.address === inputAddress);
+  }
+
+  validTransactions() {
+    return Object.values(this.transactionMap).filter(
+      transaction => Transaction.validTransaction(transaction)
+    );
+  }
+}
+
+module.exports = TransactionPool;
