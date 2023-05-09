9dde6390135fe2e65d8c09ff68304e4bb10ac6f2
nirilajay <nirilajay2317@gmail.com>
2023-05-09 21:35:33 +0530

    React router is set

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Blocks from './Blocks';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 class App extends Component {
@@ -18,14 +18,14 @@ class App extends Component {
       <div className="App">
         <img className='logo' src={logo}></img>
         <br />
-        <div>Welcome to the nrlchain...</div>
+        <div>Welcome to the blockchain...</div>
+        <br />
+        <div><Link to='/blocks'>Blocks</Link></div>
         <br />
         <div className='WalletInfo'>
           <div>Address: {address}</div>
           <div>Balance: {balance}</div>
         </div>
-        <br />
-        <Blocks />
       </div>
     );
   }
