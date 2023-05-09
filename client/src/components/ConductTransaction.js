161307c6c558a02b8844301fe8ab742e38397817
nirilajay <nirilajay2317@gmail.com>
2023-05-09 22:01:31 +0530

    post transaction and transaction pool components are set

--- client/src/components/ConductTransaction.js
+++ client/src/components/ConductTransaction.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { FormGroup, FormControl } from 'react-bootstrap';
+import { FormGroup, FormControl, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import history from '../history';
 
 class ConductTransaction extends Component {
   state = { recipient: '', amount: 0 };
@@ -13,9 +14,21 @@ class ConductTransaction extends Component {
     this.setState({ amount: Number(event.target.value) });
   }
 
-  render() {
-    console.log('this.state', this.state);
+  conductTransaction = () => {
+    const { recipient, amount } = this.state;
 
+    fetch('http://localhost:3000/api/transact', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ recipient, amount })
+    }).then(response => response.json())
+      .then(json => {
+        alert(json.message || json.type);
+        history.push('/transaction-pool');
+      });
+  }
+
+  render() {
     return (
       <div className='ConductTransaction'>
         <Link to='/'>Home</Link>
@@ -36,6 +49,14 @@ class ConductTransaction extends Component {
             onChange={this.updateAmount}
           />
         </FormGroup>
+        <div>
+          <Button
+            bsStyle="danger"
+            onClick={this.conductTransaction}
+          >
+            Submit
+          </Button>
+        </div>
       </div>
     )
   }
